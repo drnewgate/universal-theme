d9537c97082ed8828ea688e925868cdd118a27f6
drnewgate <alekseibuhtin@mail.ru>
2021-03-03 22:45:00 +0300

    create block hero

--- front-page.php
+++ front-page.php
@@ -1,31 +1,83 @@
 <?php get_header(); ?>
-<main>
+<main class='front-page-header'>
   <div class="container">
     <div class="hero">
       <div class="left">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/image.jpeg' ?>" alt="" class="post-thumb">
-        <a href="#" class="author">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="Аватарище" class="avatar">
+        <?php
+          global $post;
+
+          $myposts = get_posts([ 
+            'numberposts' => 1,
+            'category_name' => 'javascript',
+          ]);
+
+          if( $myposts ){
+            foreach( $myposts as $post ){
+              setup_postdata( $post );
+        ?>
+          <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+        <img src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>" class="post-thumb">
+        <?php $author_id = get_the_author_meta('ID'); ?>
+        <a href="<?php echo get_author_posts_url($author_id); ?>" class="author">
+          <img src="<?php echo get_avatar_url($author_id); ?>" class="avatar">
           <div class="author-bio">
-            <span class="author-name">Имя Автора</span>
+            <span class="author-name"><?php the_author(); ?></span>
             <span class="author-rank">Должность</span>
 
           </div>
         </a>
         <div class="post-text">
-          <a href="#" class="category-name">Рубрики</a>
-          <h2 class="post-title">Название поста</h2>
-            <a href="#" class="more">Читать далее</a>
+          <?php the_category(); ?>
+          <h2 class="post-title"><?php the_title(); ?></h2>
+            <a href="<?php echo get_the_permalink(); ?>" class="more">Читать далее</a>
         </div>
         <!-- /.post-text -->
+        <?php 
+            }
+          } else {
+            // Постов не найдено
+            ?> <p>Постов нет</p> <?php 
+          }
+          wp_reset_postdata(); // Сбрасываем $post
+         ?>
       </div>
       <!-- /.left -->
       <div class="right">
+        <h3 class="recommend">Рекомендуем</h3>
+        <ul class="posts-list">
+          <?php
+              global $post;
 
+              $myposts = get_posts([ 
+                'numberposts' => 5,
+                'offset' => 1,
+              ]);
+
+              if( $myposts ){
+                foreach( $myposts as $post ){
+                  setup_postdata( $post );
+           ?>
+          <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+          <li class="post">
+            <?php the_category(); ?>
+            <a class='post-permalink' href="<?php echo get_the_permalink(); ?>">
+              <h4 class="post-title"><?php the_title(); ?></h4>
+          </a>
+          </li>
+           <?php 
+                }
+              } else {
+                // Постов не найдено
+                ?> <p>Постов нет</p> <?php 
+              }
+              wp_reset_postdata(); // Сбрасываем $post
+            ?>
+        </ul>
       </div>
       <!-- /.right -->
     </div>
     <!-- .hero -->
+          
   </div>
   <!-- .conatainer -->
 </main>
--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
     // Добавление тега title 
     add_theme_support( 'title-tag' );
 
+    // Добавление миниатюр
+    add_theme_support( 'post-thumbnails', array( 'post' ) );  
+
     // добавление пользовательского логотипа
     add_theme_support( 'custom-logo', [
       'width'       => 163,
@@ -24,8 +27,8 @@ add_action( 'after_setup_theme', 'universal_theme_setup' );
 
 // Подключение стилей и скриптов 
 function enqueue_universal_style() {
-     wp_enqueue_style( 'style', get_stylesheet_uri() );
-     wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css','style', time());
-    wp_enqueue_style ('Roboto-Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'Roboto-Slab', '//fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
+  wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', time());
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
