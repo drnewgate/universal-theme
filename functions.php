997439a76ac5007b2886c44fd23683573d3aa0b8
drnewgate <alekseibuhtin@mail.ru>
2021-03-10 23:13:48 +0300

    create block with social

--- functions.php
+++ functions.php
@@ -33,15 +33,22 @@ add_action( 'after_setup_theme', 'universal_theme_setup' );
 function universal_theme_widgets_init() {
   register_sidebar(
     array(
-      'name'          => esc_html__( 'Сайдбар на главной', 'universal-exemple' ),
+      'name'          => esc_html__( 'Сайдбар на главной', 'universaltheme' ),
       'id'            => 'main-sidebar',
-      'description'   => esc_html__( 'Добавтьте виджеты сюда.', 'universal-exemple' ),
+      'description'   => esc_html__( 'Добавтьте виджеты сюда.', 'universaltheme' ),
       'before_widget' => '<section id="%1$s" class="widget %2$s">',
       'after_widget'  => '</section>',
       'before_title'  => '<h2 class="widget-title">',
       'after_title'   => '</h2>',
     )
   );
+	register_sidebar( array(
+		'name' => "Правая боковая панель сайта",
+		'id' => 'right-sidebar',
+		'description' => 'Эти виджеты будут показаны в правой колонке сайта',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>'
+	) );
 }
 add_action( 'widgets_init', 'universal_theme_widgets_init' );
 
@@ -66,6 +73,7 @@ class Downloader_Widget extends WP_Widget {
 			//add_action('wp_head', array( $this, 'add_downloader_widget_style' ) );
 		}
 	}
+	
 
 	/**
 	 * Вывод виджета во Фронт-энде
@@ -169,6 +177,157 @@ function register_downloader_widget() {
 }
 add_action( 'widgets_init', 'register_downloader_widget' );
 
+/**
+ * Добавление нового виджета Social_Widget.
+ */
+class Social_Widget extends WP_Widget {
+
+	// Регистрация виджета используя основной класс
+	function __construct() {
+		// вызов конструктора выглядит так:
+		// __construct( $id_base, $name, $widget_options = array(), $control_options = array() )
+		parent::__construct(
+			'social_widget', // ID виджета, если не указать (оставить ''), то ID будет равен названию класса в нижнем регистре: social_widget
+			'Соц. сети',
+			array( 'description' => 'Ссылки на соц сети', 'classname' => 'widget-social', )
+		);
+
+		// скрипты/стили виджета, только если он активен
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			//add_action('wp_enqueue_scripts', array( $this, 'add_downloader_widget_scripts' ));
+			//add_action('wp_head', array( $this, 'add_downloader_widget_style' ) );
+		}
+	}
+	
+	/**
+	 * Вывод виджета во Фронт-энде
+	 *
+	 * @param array $args     аргументы виджета.
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function widget( $args, $instance ) {
+		$title = $instance['title'];
+    $facebook = $instance['facebook'];
+		$twitter = $instance['twitter'];
+		$vk = $instance['vk'];
+		$youtube = $instance['youtube'];
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		if ( ! empty( $facebook ) ) {
+			echo '<a target="_blank" class="widget-social" href="' . $facebook . '">
+			<img class="widget-social-icon" src=" ' . get_template_directory_uri(). '/assets/images/facebook.svg" >
+			</a>';
+			}
+			if ( ! empty( $twitter ) ) {
+			echo '<a target="_blank" class="widget-social" href="' . $twitter . '">
+			<img class="widget-social-icon" src=" ' . get_template_directory_uri(). '/assets/images/twitter.svg" >
+			</a>';
+			}
+			if ( ! empty( $vk ) ) {
+			echo '<a target="_blank" class="widget-social" href="' . $vk . '">
+			<img class="widget-social-icon" src=" ' . get_template_directory_uri(). '/assets/images/vk.svg" >
+			</a>';
+			}
+			if ( ! empty( $youtube ) ) {
+			echo '<a target="_blank" class="widget-social" href="' . $youtube . '">
+			<img class="widget-social-icon" src=" ' . get_template_directory_uri(). '/assets/images/youtube.svg" >
+			</a>';
+			}
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Админ-часть виджета
+	 *
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function form( $instance ) {
+		$title = @ $instance['title'] ?: 'Наши соцсети';
+    $facebook = @ $instance['facebook'] ?: '#';
+		$twitter = @ $instance['twitter'] ?: '#';
+		$vk = @ $instance['vk'] ?: '#';
+		$youtube = @ $instance['youtube'] ?: '#';
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Заголовок:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+    <p>
+			<label for="<?php echo $this->get_field_id( 'facebook' ); ?>"><?php _e( 'Facebook:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'facebook' ); ?>" name="<?php echo $this->get_field_name( 'facebook' ); ?>" type="text" value="<?php echo esc_attr( $facebook ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'twitter' ); ?>"><?php _e( 'Twitter:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'twitter' ); ?>" name="<?php echo $this->get_field_name( 'twitter' ); ?>" type="text" value="<?php echo esc_attr( $twitter ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'vk' ); ?>"><?php _e( 'Vk:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'vk' ); ?>" name="<?php echo $this->get_field_name( 'vk' ); ?>" type="text" value="<?php echo esc_attr( $vk ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'youtube' ); ?>"><?php _e( 'Youtube:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'youtube' ); ?>" name="<?php echo $this->get_field_name( 'youtube' ); ?>" type="text" value="<?php echo esc_attr( $youtube ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Сохранение настроек виджета. Здесь данные должны быть очищены и возвращены для сохранения их в базу данных.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance новые настройки
+	 * @param array $old_instance предыдущие настройки
+	 *
+	 * @return array данные которые будут сохранены
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    $instance['facebook'] = ( ! empty( $new_instance['facebook'] ) ) ? strip_tags( $new_instance['facebook'] ) : '';
+		$instance['twitter'] = ( ! empty( $new_instance['twitter'] ) ) ? strip_tags( $new_instance['twitter'] ) : '';
+		$instance['vk'] = ( ! empty( $new_instance['vk'] ) ) ? strip_tags( $new_instance['vk'] ) : '';
+		$instance['youtube'] = ( ! empty( $new_instance['youtube'] ) ) ? strip_tags( $new_instance['youtube'] ) : '';
+		return $instance;
+	}
+
+	// скрипт виджета
+	function add_social_widget_scripts() {
+		// фильтр чтобы можно было отключить скрипты
+		if( ! apply_filters( 'show_my_widget_script', true, $this->id_base ) )
+			return;
+
+		$theme_url = get_stylesheet_directory_uri();
+
+		wp_enqueue_script('my_widget_script', $theme_url .'/my_widget_script.js' );
+	}
+
+	// стили виджета
+	function add_social_widget_style() {
+		// фильтр чтобы можно было отключить стили
+		if( ! apply_filters( 'show_my_widget_style', true, $this->id_base ) )
+			return;
+		?>
+		<style type="text/css">
+			.my_widget a{ display:inline; }
+		</style>
+		<?php
+	}
+
+} 
+// конец класса Social_Widget
+
+// регистрация Social_Widget в WordPress
+function register_social_widget() {
+	register_widget( 'Social_Widget' );
+}
+add_action( 'widgets_init', 'register_social_widget' );
+
+
 // Подключение стилей и скриптов 
 function enqueue_universal_style() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
@@ -176,6 +335,17 @@ function enqueue_universal_style() {
   wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', time());
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+
+## изменяем настройки облака тегов
+add_filter( 'widget_tag_cloud_args', 'edit_widget_tag_cloud_args');
+function edit_widget_tag_cloud_args($args) {
+	$args['unit'] = 'px';
+	$args['smallest'] = '14';
+	$args['largest'] = '14';
+	$args['number'] = '8';
+
+	return $args;
+}
 
 ## отключаем создание миниатюр файлов для указанных размеров
 add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
